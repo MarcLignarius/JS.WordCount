05503a21cf7e151c26b12fb3801dbd56c1afec20
Marc Davies <marcdaviesriot@gmail.com>
2020-01-16 15:14:16 -0800

    push split user sentence into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 $(function() {
-    $("#userForm").submit(function(event) {
-        event.preventDefault();
-        let userInput = $("#userInput").val().toLowerCase().replace(/[\.,-\/#!$%"\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, "");;
-        console.log("userInput", userInput);
-        let splitInput = userInput.split(" ");
-        console.log("splitInput", splitInput);
-    })
+  $("#userForm").submit(function(event) {
+    event.preventDefault();
+    let userInput = $("#userInput")
+      .val()
+      .toLowerCase()
+      .replace(/[\.,-\/#!$%"\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, "");
+    console.log("userInput", userInput);
+    let wordArray = [];
+    wordArray.push(userInput.split(" "));
+    console.log("wordArray", wordArray);
+  });
 });
