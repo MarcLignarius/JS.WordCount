baad1ddc0471970117ab30311ae4986dbafaf152
Marc Davies <marcdaviesriot@gmail.com>
2020-01-22 13:49:33 -0800

    add script to log word count and log it to the console

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,15 @@ $(function() {
     console.log("userInput", userInput);
     let wordArray = userInput.split(" ");
     console.log("wordArray", wordArray);
+    let countedWords = [];
+    for (i=0; i < wordArray.length; i++) {
+      let word = wordArray[i];
+      if (!countedWords[word]) {
+        countedWords[word] = 1;
+      } else {
+        countedWords[word]++;
+      }
+    }
+    console.log(countedWords);
   });
 });
