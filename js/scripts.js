828fdc880fe8e6e90b2433d9ccfeaa3c282b56be
Marc Davies <marcdaviesriot@gmail.com>
2020-01-24 13:54:31 -0800

    remove commented out code

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,11 @@ $(function() {
   });
 
   function inputToArray(userInput) {
-    // split string by spaces (including spaces, tabs, and newlines)
     var inputArray = userInput.split(/\s+/);
     return inputArray;
   }
 
   function countWords(inputArray) {
-    // create map for word counts
     var wordCount = {};
     inputArray.forEach(function(key) {
       if (wordCount.hasOwnProperty(key)) {
@@ -31,7 +29,6 @@ $(function() {
   }
 
   function sortByCount(wordCount) {
-    // sort by count in descending order
     var sortedCount = [];
     sortedCount = Object.keys(wordCount).map(function(key) {
       return {
@@ -55,29 +52,3 @@ $(function() {
   return output;
   }
 });
-
-// $(function() {
-//   $("#userForm").submit(function(event) {
-//     event.preventDefault();
-//     let userInput = $("#userInput")
-//       .val()
-//       .toLowerCase()
-//       .replace(/[\.,-\/#!$%"\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, "");
-//     console.log("userInput", userInput);
-//     let wordArray = userInput.split(/\s+/);
-//     console.log("wordArray", wordArray);
-//     let countedWords = {};
-//     wordArray.forEach(function(key) {
-//       if (countedWords.hasOwnProperty(key)) {
-//         countedWords[key]++;
-//       } else {
-//         countedWords[key] = 1;
-//       }
-//       return countedWords;
-//     })
-//     console.log("countedWords", countedWords);
-//     let result = JSON.stringify(countedWords, null, 4);
-//     console.log("result", result);
-//     $("#output").text(result);
-//   });
-// });
