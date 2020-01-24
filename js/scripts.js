c6e4538f5f47028f6438df4e988afc579c469ce5
Marc Davies <marcdaviesriot@gmail.com>
2020-01-24 10:34:58 -0800

    trying another method, restart at logging sanitized user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,37 @@
 $(function() {
-  $("#userForm").submit(function(event) {
-    event.preventDefault();
-    let userInput = $("#userInput")
-      .val()
-      .toLowerCase()
-      .replace(/[\.,-\/#!$%"\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, "");
-    console.log("userInput", userInput);
-    let wordArray = userInput.split(/\s+/);
-    console.log("wordArray", wordArray);
-    let countedWords = {};
-    wordArray.forEach(function(key) {
-      if (countedWords.hasOwnProperty(key)) {
-        countedWords[key]++;
-      } else {
-        countedWords[key] = 1;
-      }
-      return countedWords;
-    })
-    console.log("countedWords", countedWords);
-    let result = JSON.stringify(countedWords, null, 4);
-    console.log("result", result);
-    $("#output").text(result);
+    $("#userForm").submit(function(event) {
+      event.preventDefault();
+      let userInput = $("#userInput")
+        .val()
+        .toLowerCase()
+        .replace(/[\.,-\/#!$%"\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, "");
+      console.log("userInput", userInput);
   });
 });
+
+// $(function() {
+//   $("#userForm").submit(function(event) {
+//     event.preventDefault();
+//     let userInput = $("#userInput")
+//       .val()
+//       .toLowerCase()
+//       .replace(/[\.,-\/#!$%"\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, "");
+//     console.log("userInput", userInput);
+//     let wordArray = userInput.split(/\s+/);
+//     console.log("wordArray", wordArray);
+//     let countedWords = {};
+//     wordArray.forEach(function(key) {
+//       if (countedWords.hasOwnProperty(key)) {
+//         countedWords[key]++;
+//       } else {
+//         countedWords[key] = 1;
+//       }
+//       return countedWords;
+//     })
+//     console.log("countedWords", countedWords);
+//     let result = JSON.stringify(countedWords, null, 4);
+//     console.log("result", result);
+//     $("#output").text(result);
+//   });
+// });
+
